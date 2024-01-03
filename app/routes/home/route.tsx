41cca23796445e12819d23f5cc4fc44760cef6a7
otaki0413 <takida0413@gmail.com>
2024-01-03 14:46:53 +0900

    ✨ ホームの改善

--- app/routes/home/route.tsx
+++ app/routes/home/route.tsx
@@ -27,30 +27,33 @@ export async function action({ request }: ActionFunctionArgs) {
 }
 
 export default function Projects() {
-  const { boards } = useLoaderData<typeof loader>();
   return (
     <div className="h-full bg-gray-50">
-      <nav className="flex flex-wrap gap-8 p-8">
-        <NewBoard />
-        {boards.map((board) => (
-          <Tile key={board.id}>
-            <Link
-              to={String(board.id)}
-              className="block h-full w-full rounded border-b-8 p-4 shadow"
-              style={{ borderColor: board.color }}
-            >
-              <div className="font-bold">{board.name}</div>
-            </Link>
-          </Tile>
-        ))}
-      </nav>
+      <NewBoard />
+      <Boards />
     </div>
   );
 }
 
-function Tile({ children }: { children: React.ReactNode }) {
-  // eslint-disable-next-line react/no-children-prop
-  return <div className="h-40 w-60 bg-white" children={children} />;
+function Boards() {
+  const { boards } = useLoaderData<typeof loader>();
+  return (
+    <div className="p-8">
+      <h2>Boards</h2>
+      <nav className="flex flex-wrap gap-8">
+        {boards.map((board) => (
+          <Link
+            key={board.id}
+            to={`/board/${board.id}`}
+            className="block h-40 w-60 rounded border-b-8 bg-white p-4 shadow transition-transform hover:scale-105 hover:shadow-lg"
+            style={{ borderColor: board.color }}
+          >
+            <div className="font-bold">{board.name}</div>
+          </Link>
+        ))}
+      </nav>
+    </div>
+  );
 }
 
 function NewBoard() {
@@ -64,57 +67,52 @@ function NewBoard() {
   }, [fetcher]);
 
   return (
-    <Tile>
-      <fetcher.Form
-        method="post"
-        ref={ref}
-        className="h-full border border-gray-100 p-4"
-      >
-        <div>
-          <label
-            htmlFor="board-name"
-            className="block text-sm font-medium leading-6 text-gray-900"
-          >
-            New Board{" "}
-            {fetcher.data?.ok === false ? (
-              <span className="text-red-brand">{fetcher.data.message}</span>
-            ) : null}
-          </label>
-          <div className="mt-2">
-            <input
-              id="board-name"
-              name="name"
-              type="text"
-              required
-              className="focus:ring-indigo-600 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
-            />
-          </div>
+    <fetcher.Form method="post" ref={ref} className="max-w-md p-8">
+      <div>
+        <h2 className="mb-2 text-xl font-bold">New Board</h2>
+        <label
+          htmlFor="board-name"
+          className="block text-sm font-medium leading-6 text-gray-900"
+        >
+          Name{" "}
+          {fetcher.data?.ok === false ? (
+            <span className="text-brand-red">{fetcher.data.message}</span>
+          ) : null}
+        </label>
+        <div className="mt-2">
+          <input
+            id="board-name"
+            name="name"
+            type="text"
+            required
+            className="focus:ring-indigo-600 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
+          />
         </div>
+      </div>
 
-        <div className="mt-4 flex items-center gap-4">
-          <div className="flex items-center gap-1">
-            <label
-              htmlFor="board-color"
-              className="text-sm font-medium leading-6 text-gray-900"
-            >
-              Color
-            </label>
-            <input
-              id="board-color"
-              name="color"
-              type="color"
-              defaultValue="#e0e0e0"
-              className="border p-0"
-            />
-          </div>
-          <button
-            type="submit"
-            className="hover:bg-indigo-500 focus-visible:outline-indigo-600 flex w-full justify-center rounded-md bg-blue-brand px-1 py-1 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+      <div className="mt-4 flex items-center gap-4">
+        <div className="flex items-center gap-1">
+          <label
+            htmlFor="board-color"
+            className="text-sm font-medium leading-6 text-gray-900"
           >
-            Create
-          </button>
+            Color
+          </label>
+          <input
+            id="board-color"
+            name="color"
+            type="color"
+            defaultValue="#e0e0e0"
+            className="border p-0"
+          />
         </div>
-      </fetcher.Form>
-    </Tile>
+        <button
+          type="submit"
+          className="hover:bg-indigo-500 bg-indigo-400 focus-visible:outline-indigo-600 flex w-full justify-center rounded-md bg-blue-brand px-1 py-1 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+        >
+          Create
+        </button>
+      </div>
+    </fetcher.Form>
   );
 }
